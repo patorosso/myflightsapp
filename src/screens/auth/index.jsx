b69f07f4f48fb51b9211bc2cac6a00c3442266a1
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-03-02 12:57:43 -0300

    added navigation auth and screen

---
+++ src/screens/auth/index.jsx
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+import {
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from 'react-native';
+import { useDispatch } from 'react-redux';
+
+import { styles } from './styles';
+import { colors } from "../../constants";
+
+import { signIn, signUp } from '../../store/actions';
+
+const Auth = ({ navigation }) => {
+  const dispatch = useDispatch();
+  const [isLogin, setIsLogin] = useState(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const title = isLogin ? 'Login' : 'Register';
+  const message = isLogin ? "Don't have an account?" : 'Already have an account?';
+  const messageButton = isLogin ? 'Login' : 'Register';
+
+  const onHandlerSubmit = () => {
+    dispatch(isLogin ? signIn(email, password) : signUp(email, password));
+  };
+  return (
+    <KeyboardAvoidingView
+      style={styles.keybordContainer}
+      behavior={Platform.OS === 'android' ? 'height' : 'padding'}
+      enabled>
+      <View style={styles.container}>
+        <View style={styles.content}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.label}>Email</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="enter your email"
+            placeholderTextColor={colors.text}
+            autoCapitalize="none"
+            autoCorrect={false}
+            onChangeText={(text) => setEmail(text)}
+            value={email}
+          />
+          <Text style={styles.label}>Password</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="enter your password"
+            placeholderTextColor={colors.text}
+            secureTextEntry
+            autoCapitalize="none"
+            autoCorrect={false}
+            onChangeText={(text) => setPassword(text)}
+            value={password}
+          />
+          <View style={styles.buttonContainer}>
+            <Button title={messageButton} color={colors.primary} onPress={onHandlerSubmit} />
+            <View style={styles.prompt}>
+              <TouchableOpacity style={styles.promptButton} onPress={() => setIsLogin(!isLogin)}>
+                <Text style={styles.promptMessage}>{message}</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
+  );
+};
+
+export default Auth;
--- src/screens/index.js
+++ src/screens/index.js
@@ -3,3 +3,4 @@ export {default as Flights} from './flights';
 export {default as FlightDetail} from './flight-detail';
 export {default as Settings} from './settings';
 export {default as User} from './user';
+export {default as Auth} from './auth';
