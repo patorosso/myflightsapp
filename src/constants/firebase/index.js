31890033c038c55ed57e1fa6bc29312696dc57ca
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-03-02 12:50:07 -0300

    added api_key and sign in and up urls

--- src/constants/firebase/index.js
+++ src/constants/firebase/index.js
@@ -1,1 +1,4 @@
 export const REALTIME_DATABASE_URL = 'https://myflightsapp-f7f3b-default-rtdb.firebaseio.com';
+export const API_KEY = 'AIzaSyBCKGDJqOKHODbp0551JRShc_TP4RNAPtI';
+export const URL_AUTH_SIGN_IN = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`;
+export const URL_AUTH_SIGN_UP = `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`;
