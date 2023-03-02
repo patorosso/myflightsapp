15f33f364dbf7dadb20f472f3b8003a8de8ab00e
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-03-02 14:10:57 -0300

    fixed auth id: 1

--- src/store/actions/auth.action.js
+++ src/store/actions/auth.action.js
@@ -5,30 +5,34 @@ const {SIGN_UP, SIGN_IN} = authTypes;
 
 export const signUp = (email, password) => {
     return async (dispatch) => {
-        try {
-            const response = await fetch(URL_AUTH_SIGN_UP, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({email, password, returnSecureToken: true})
-            });
-
-            if(!response.ok){
-                throw new Error('Something went wrong!');
-            }
-
-            const data = await response.json();
-
-            dispatch({
-                type: SIGN_UP,
-                token: data.idToken,
-                userId: data.localId,
-            })
-        } catch (error) {
-            throw error;
+      try {
+        const response = await fetch(URL_AUTH_SIGN_UP, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            email,
+            password,
+            returnSecureToken: true,
+          }),
+        });
+  
+        if (!response.ok) {
+          throw new Error('Something went wrong!');
         }
+  
+        const data = await response.json();
+  
+        dispatch({
+          type: SIGN_UP,
+          token: data.idToken,
+          userId: data.localId,
+        });
+      } catch (error) {
+        throw error;
+      }
     };
-};
+  };
 
 export const signIn = (email, password) => {};
