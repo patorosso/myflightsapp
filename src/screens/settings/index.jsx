caaced3d20a3789ceed865c6d5315d683dd7c045
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-03-06 20:24:44 -0300

    home matrix done on settings

--- src/screens/settings/index.jsx
+++ src/screens/settings/index.jsx
@@ -1,46 +1,64 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
-import { useState } from "react";
-import {  FLIGHT_API_KEY } from "../../constants/flight_api";
+import { View, Text} from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
+import { styles } from "./styles";
+import { colors } from "../../constants";
+import { Ionicons } from "@expo/vector-icons";
 
 
 const Settings = () => {
-    const [flightStatus, setFlightStatus] = useState(null);
-
-    function getFlightStatus() {
-      const apiKey = FLIGHT_API_KEY;
-      const flightNumber = 'AZ681';
-  
-      fetch(`https://app.goflightlabs.com/flights?access_key=${apiKey}&flightIata=${flightNumber}`, {
-        headers: {
-          'Authorization': `Bearer ${apiKey}`
-        },
-        
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log(data);
-        setFlightStatus(data);
-      })
-      .catch(error => console.error(error));
-    }
-  
     return (
-      <View style={{flex:1}}>
-        
-        <Button title="Get Flight Status" onPress={getFlightStatus} />
-        
-        {flightStatus && (
-            
-          <View>
-                <Text>Flight Number: {flightStatus.data[0].flight.iataNumber}</Text>
-                <Text>Status: {flightStatus.data[0].status}</Text>
-                <Text>Departure Airport: {flightStatus.data[0].departure.iataCode}</Text>
-                <Text>Arrival Airport: {flightStatus.data[0].arrival.iataCode}</Text>
-          </View>
-        )}
-        
+      <View style={styles.container}>
+        <LinearGradient  
+        colors={[  colors.darkblue, colors.primary ]} 
+        style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+            <View style={styles.iconContainer}>
+
+              <View style={{flex:0.5, flexDirection:'row'}}>
+                
+                <View style={{flex:0.5,borderBottomColor:'white',borderBottomWidth: 3,borderRightColor:'white',borderRightWidth:3,justifyContent: 'center',paddingRight:10}}>
+                <Ionicons
+                    name={'search'}
+                    size={80}
+                    color={colors.white}
+                    style={{alignSelf: 'center'}}
+                  />
+                  <Text style={{fontFamily: 'Nunito-Bold',color: 'white',fontSize: 13,alignSelf: 'center', paddingVertical: 10}}>Search by Flight</Text>
+                </View>
+                <View style={{flex:0.5,borderBottomColor:'white',borderBottomWidth: 3,borderLeftColor:'white',borderLeftWidth:3,justifyContent: 'center',paddingLeft:10}}>
+                <Ionicons
+                    name={'book'}
+                    size={80}
+                    color={colors.white}
+                    style={{alignSelf: 'center'}}
+                  />
+                  <Text style={{fontFamily: 'Nunito-Bold',color: 'white',fontSize: 13,alignSelf: 'center', paddingVertical: 10}}>History</Text>
+                </View>
+              </View>
+              
+              <View style={{flexDirection: 'row',flex:0.5}}>
+                <View style={{flex:0.5,borderTopColor:'white',borderTopWidth: 3,borderRightColor:'white',borderRightWidth:3,justifyContent: 'center',paddingRight:10}}>
+                <Ionicons
+                    name={'settings'}
+                    size={80}
+                    color={colors.white}
+                    style={{alignSelf: 'center', paddingVertical: 10}}
+                  />
+                  <Text style={{fontFamily: 'Nunito-Bold',color: 'white',fontSize: 13,alignSelf: 'center'}}>Search by Flight</Text>
+                </View>
+                <View style={{flex:0.5,borderTopColor:'white',borderTopWidth: 3,borderLeftColor:'white',borderLeftWidth:3,justifyContent: 'center',paddingLeft:10}}>
+                <Ionicons
+                    name={'home'}
+                    size={80}
+                    color={colors.white}
+                    style={{alignSelf: 'center', paddingVertical: 10}}
+                  />
+                  <Text style={{fontFamily: 'Nunito-Bold',color: 'white',fontSize: 13,alignSelf: 'center'}}>Search by Flight</Text>
+                </View>
+              </View>
+
+            </View>
+        </LinearGradient>
       </View>
     );
   }
--- src/screens/settings/styles.js
+++ src/screens/settings/styles.js
@@ -1,16 +1,26 @@
 import { StyleSheet } from "react-native";
+import { colors } from "../../constants";
 
 
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: '#fff',
         justifyContent: 'center',
-        alignItems: 'center',
     },
-    textContainer: {
-        fontFamily: "Nunito-Medium",
-        textAlign: 'center',
-        fontSize: 20
-    }
+    iconContainer: {
+        
+        width: '80%',
+        height: '50%',
+        
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+
+        
+    },
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        paddingVertical: 70,
+    },
+    
 });
