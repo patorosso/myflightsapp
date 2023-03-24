5f65d29df29ac17b48c6817a9da843e897beab62
Patricio Rosso <patriciosimonrosso@gmail.com>
2023-03-24 20:04:54 -0300

    added schedule-item component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as FlightInfo } from './flight-info/index';
 export { default as FlightInfoShort } from './flight-info-short/index.jsx';
 export { default as CustomModal} from './custom-modal/index';
+export {default as ScheduleItem} from './schedule-item/index';
---
+++ src/components/schedule-item/index.jsx
@@ -0,0 +1,13 @@
+import { View, Text, TouchableOpacity} from 'react-native';
+
+
+
+const ScheduleItem = () => {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default ScheduleItem;
